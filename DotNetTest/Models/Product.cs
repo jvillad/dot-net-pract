4140f43146c7e1a9b3a16e77048f363061c96455
jvillad <jyvillad@gmail.com>
2023-02-10 22:42:04 +1100

    Add project files.

---
+++ DotNetTest/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNetTest.Models
+{
+    public class Product
+    {
+        public string? Id { get; set; }
+        public int? Maker { get; set; }
+
+        // mapping img from json to int Image
+        [JsonPropertyName("img")]
+        public int Image { get; set; }
+        public int? Url { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int[]? Ratings { get; set; }
+
+        // taking all object information and converting it back json 
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+
+
+
+    }
+}
---
+++ DotNetTest/Services/JsonFileProductService.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using DotNetTest.Models;
+
+namespace ContosoCrafts.WebSite.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            // handles the slashes '/' ../../../wwwroot/data/products.json
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
